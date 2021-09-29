fc58423f7032959b09117988356a44c59a2c2fe4
Santiago Hoyos <zahono@gmail.com>
2021-09-28 20:45:27 -0500

    Primer commit, explicacion de BlocProvider y BlocBuilder

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_bloc_concepts
+
+Curso sobre el patrón BLoC en youtube de [Flutterly](https://www.youtube.com/channel/UC5PYcSe3to4mtm3SPCUmjvw), link del curso en youtube:
+
+[The Best Flutter Bloc Complete Course - Visualise, Understand, Learn & Practice Bloc Concepts](https://www.youtube.com/watch?v=THCkkQ-V1-8)
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_concepts/src/core/logic/counter/counter_cubit.dart';
+
+void main() {
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: const MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    CounterCubit bloc = BlocProvider.of<CounterCubit>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  '${state.counterValue}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => bloc.decrement(),
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                FloatingActionButton(
+                  onPressed: () => bloc.increment(),
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
